bcf4f1c34009284e0760aa7120f28b846b05ce73
Jongmin Kim <kdzlvaids@gmail.com>
2016-03-18 16:08:35 +0900

    Rename 'shift_right()' to 'count_sum()'

--- excercise03/02.cpp
+++ excercise03/02.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void trim_out_of_range(void);
-int shift_right(const int idx, const int sum = 0);
+int count_sum(const int idx, const int sum = 0);
 
 int *list;
 int size;
@@ -21,7 +21,7 @@ int main(void)
     printf("Enter k= "); scanf("%d", &sum);
 
     trim_out_of_range();
-    printf("Result: %d\n", shift_right(size - 1));
+    printf("Result: %d\n", count_sum(size - 1));
 
     free(list);
 
@@ -38,10 +38,10 @@ void trim_out_of_range(void)
         }
 }
 
-int shift_right(const int idx, const int sum)
+int count_sum(const int idx, const int sum)
 {
     if(idx < 0 && sum == ::sum) return 1;
     if(idx < 0) return 0;
 
-    return shift_right(idx - 1, sum + list[idx]) + shift_right(idx - 1, sum);
+    return count_sum(idx - 1, sum + list[idx]) + count_sum(idx - 1, sum);
 }
