d4869180f3cb6561f32c3c082c91e5d80d4523fd
Jongmin Kim <kdzlvaids@gmail.com>
2016-03-24 11:31:21 +0900

    Add body of Quick sort

--- excercise04/sort/quick.cpp
+++ excercise04/sort/quick.cpp
@@ -1,6 +1,42 @@
+#include <string.h> /* memcpy() */
+#include <stdlib.h> /* rand() */
+
 #include "quick.hpp"
 #include "swap.hpp"
 
 void quick(int *data, const int size_of_data)
 {
+    return quick(data, 0, size_of_data - 1);
+}
+
+void quick(int *data, const int start, const int end)
+{
+    int size = end - start + 1;
+    if(size <= 1) return; /* Base case */
+
+    /* Conquer */
+    int pivot = start + rand() % size; /* Set pivot */
+
+    int lss[size] = {0, }; /* Memory which stores less than data[pivot] */
+    int gtr[size] = {0, }; /*                     greater               */
+    int pos_lss = 0, pos_gtr = 0;
+
+    for(int pos = start; pos <= end; ++pos)
+    {
+        if(pos == pivot) continue; /* Ignore pivot */
+
+        if(data[pos] < data[pivot])
+            lss[pos_lss++] = data[pos];
+        else
+            gtr[pos_gtr++] = data[pos];
+    }
+
+    /* Copy the sorted data */
+    data[start + pos_lss] = data[pivot];
+    memcpy(data + start, lss, sizeof(int) * pos_lss);
+    memcpy(data + start + pos_lss + 1, gtr, sizeof(int) * pos_gtr);
+
+    /* Divide */
+    quick(data, start, start + pos_lss - 1);
+    quick(data, start + pos_lss + 1, end);
 }
--- excercise04/sort/quick.hpp
+++ excercise04/sort/quick.hpp
@@ -2,6 +2,7 @@
 #define __SORT_QUICK_HPP   1
 
 void quick(int *data, const int size_of_data);
+void quick(int *data, const int start, const int end);
 
 #endif /* !__SORT_QUICK_HPP */
 
