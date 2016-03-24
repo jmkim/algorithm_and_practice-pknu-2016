d20af428e1db9f6b7a653886024e89380862c663
Jongmin Kim <kdzlvaids@gmail.com>
2016-03-24 22:14:39 +0900

    Add test layout

--- excercise04/sort/quick.cpp
+++ excercise04/sort/quick.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "quick.hpp"
 #include "swap.hpp"
 
@@ -8,6 +10,14 @@ void quick(int *data, const int size_of_data)
 
 void quick(int *data, const int start, const int end)
 {
+    int size = end - start + 1;
+
+#ifdef TESTLAYOUT
+    printf("START %d : END %d : PIVOT %d\n", start, end, data[end]);
+    for(int i = start; i <= end; ++i) printf(" %2d  ", data[i]);
+    printf("\n");
+#endif
+
     if(start >= end) return; /* Base case */
 
     /* Conquer */
@@ -19,10 +29,28 @@ void quick(int *data, const int start, const int end)
         while(ps < pe && data[ps] < data[pivot]) ++ps;
         while(ps < pe && data[pe] >= data[pivot]) --pe;
 
+#ifdef TESTLAYOUT
+        for(int i = start; i <= end; ++i)
+        {
+            if(i == ps && i == pe)
+                printf("(%2d) ", data[i]);
+            else if(i == ps || i == pe)
+                printf("[%2d] ", data[i]);
+            else
+                printf(" %2d  ", data[i]);
+        }
+        printf("  (%d, %d)\n", ps, pe);
+#endif
+
         if(ps < pe) swap(data[ps], data[pe]);
     }
 
     if(data[pe] > data[pivot]) swap(data[pe], data[pivot]);
+
+#ifdef TESTLAYOUT
+    for(int i = start; i <= end; ++i) printf(" %2d  ", data[i]);
+    printf("\n");
+#endif
 
     /* Divide */
     quick(data, start, pe);
