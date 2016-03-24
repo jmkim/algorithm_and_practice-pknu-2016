0ad3487e5eef96c01a316bde7cbba8a60a16489f
Jongmin Kim <kdzlvaids@gmail.com>
2016-03-24 11:31:38 +0900

    Add main_for_testing.cpp

---
+++ excercise04/sort/main_for_testing.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../make_random_data/make_random_data.hpp"
+#include "../sort/merge.hpp"
+#include "../sort/quick.hpp"
+#include "../sort/selection.hpp"
+#include "../sort/insertion.hpp"
+#include "../sort/bubble.hpp"
+
+#define SORT    quick
+#define SIZE    10
+
+int main(void)
+{
+    int *data = make_random_data(SIZE);
+
+    for(int i = 0; i < SIZE; ++i) printf("%d\n", data[i]);
+    putchar('\n');
+
+    SORT(data, SIZE);
+
+    for(int i = 0; i < SIZE; ++i) printf("%d\n", data[i]);
+
+    free_random_data(data);
+
+    return 0;
+}
