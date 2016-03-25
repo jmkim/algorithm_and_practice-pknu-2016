8d33f4cfb2382983bb49293c26ca336b8bdcfdd0
Jongmin Kim <kdzlvaids@gmail.com>
2016-03-25 10:05:38 +0900

    Add solution of prob 3

---
+++ excercise05/03.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "../excercise04/make_random_data/make_random_data.hpp"
+#include "../excercise04/sort/quick.hpp"
+
+int main(void)
+{
+    int k;
+    printf("Enter k= ");
+    scanf("%d", &k);
+
+    {
+    }
+
+    return 0;
+}
