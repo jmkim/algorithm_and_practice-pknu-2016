d0da4673b590c54fbff67948894a7002bdbd3147
Jongmin Kim <jmkim@pukyong.ac.kr>
2016-03-28 21:55:00 +0900

    Add a body of Heapsort

--- excercise04/sort/heapsort.hpp
+++ excercise04/sort/heapsort.hpp
@@ -2,6 +2,8 @@
 #define __SORT_HEAP_HPP   1
 
 void heapsort(int *data, const int size_of_data);
+void do_heapsort(int *data, const int end);
+void make_binary_max_heap(int *data, const int end);
 
 #endif /* !__SORT_HEAP_HPP */
 
