a50f659bf2ceb12c6bd22697c364b8713c08c6b9
Jongmin Kim <jmkim@pukyong.ac.kr>
2016-03-28 22:21:46 +0900

    Rename a function of Binary heap
    * binary_max_heap() -> convert_to_binary_max_heap()

--- excercise04/sort/heap.cpp
+++ excercise04/sort/heap.cpp
@@ -1,9 +1,9 @@
 #include "heap.hpp"
 #include "swap.hpp"
 
-void binary_max_heap(int *data, const int end)
+void convert_to_binary_max_heap(int *data, const int size_of_data)
 {
-    for(int i = end; i > 0; --i)
+    for(int i = size_of_data - 1; i > 0; --i)
     {
         for(int k = i; k > 0; k = (k - 1) / 2)
             if(data[i] > data[(k - 1) / 2]) swap(data[i], data[(k - 1) / 2]);
--- excercise04/sort/heap.hpp
+++ excercise04/sort/heap.hpp
@@ -1,6 +1,6 @@
 #ifndef __SORT_HEAP_HPP
 #define __SORT_HEAP_HPP   1
 
-void binary_max_heap(int *data, const int end);
+void convert_to_binary_max_heap(int *data, const int size_of_data);
 
 #endif /* !__SORT_HEAP_HPP */
