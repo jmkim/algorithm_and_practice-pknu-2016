80609ebb666c970bc150c86d41121291feb6ef13
Jongmin Kim <kdzlvaids@gmail.com>
2016-03-29 19:04:38 +0900

    Add Priority Queue

---
+++ excercise06/priority_queue/priority_queue.hpp
@@ -0,0 +1,8 @@
+#ifndef __PRIORITY_QUEUE_HPP
+#define __PRIORITY_QUEUE_HPP    1
+
+int push_priority_queue(int *data, const int capacity, int count, const int input);
+int pop_priority_queue(int *data, int count);
+int peak_priority_queue(int *data);
+
+#endif /* !__PRIORITY_QUEUE_HPP */
