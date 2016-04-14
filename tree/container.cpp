8954aa733c814d96bc6f41786757b50a5ea5169e
Jongmin Kim <jmkim@pukyong.ac.kr>
2016-04-15 00:00:37 +0900

    Container: Add a new tree (Red-black tree)

--- tree/container.cpp
+++ tree/container.cpp
@@ -12,7 +12,7 @@ namespace container
 
     template<class ELEMENT, typename DATA> class node
     {
-    private:
+    protected:
         ELEMENT *me;
         node *left;
         node *right;
@@ -37,7 +37,7 @@ namespace container
 
     template<class ELEMENT, typename DATA> class node_p : public node
     {
-    private:
+    protected:
         node_p *parent;
 
     public:
@@ -59,6 +59,34 @@ namespace container
 
         void set_parent(const node_p *parent) { this->right = right; }
         node_p *get_parent(void) const { return parent; }
+    };
+
+    template<class ELEMENT, typename DATA> class node_rb : public node_p
+    {
+    protected:
+        bool is_color_black;
+
+    public:
+        node_p(const DATA &key) : is_color_black(false)
+        {
+            me = new ELEMENT(key);
+            left = NULL;
+            right = NULL;
+            parent = NULL;
+        }
+        node_p(const DATA &key, const node_p *parent) : is_color_black(false)
+        {
+            me = new ELEMENT(key);
+            left = NULL;
+            right = NULL;
+            this->parent = parent;
+        }
+        ~node_p(void) { delete me; }
+
+        void set_black(void) { is_color_black = true; }
+        void set_red(void) { is_color_black = false; }
+        bool is_black(void) const { return is_color_black; }
+        bool is_red(void) const { return ! is_color_black; }
     };
 
     template<class NODE, typename DATA> class binary_tree
@@ -151,7 +179,11 @@ namespace container
 
         void insert(const NODE *node)
         {
-            if(is_empty()) root = node;
+            if(is_empty())
+            {
+                root = node;
+                return;
+            }
 
             NODE *n = root;
             NODE *p = NULL; /* One step previous node of n */
@@ -178,16 +210,13 @@ namespace container
         {
             if(node == NULL) return; /* Not exists */
 
-            if(node->get_left != NULL && node->get_right != NULL)
+            if(node->get_left() != NULL && node->get_right() != NULL)
             {
                 NODE *s = get_inorder_successor(node);
                 if(s->get_parent() != NULL) s->get_parent()->set_right(s->get_right());
 
-                s->set_left(node->get_left());
-                s->set_right(node->get_right());
-
                 if(node->get_parent() == NULL)
-                    root = n;
+                    root = s;
                 else
                 {
                     if(node->get_parent()->get_left() == node)
@@ -195,6 +224,10 @@ namespace container
                     else
                         node->get_parent()->set_right(s);
                 }
+
+                s->set_left(node->get_left());
+                s->set_right(node->get_right());
+                s->set_parent(node->get_parent());
             }
             else
             {
@@ -208,8 +241,6 @@ namespace container
                         node->get_parent()->set_right(node->get_right());
                 }
             }
-
-            destroy_node(node);
         }
 
     public:
@@ -227,6 +258,158 @@ namespace container
         }
 
         void insert(const DATA &key) { insert(create_node(key)); }
-        void delete(const DATA &key) { delete(search(key)); }
+        void delete(const DATA &key) { NODE *node = search(key); delete(node); destroy_node(node); }
+    };
+
+    template<class NODE, typename DATA> class red_black_tree : public binary_search_tree<NODE, DATA>
+    {
+    private:
+        NODE *nil;
+
+        red_black_tree(void)
+        {
+            nil = create_node("");
+            nil->set_left(nil);
+            nil->set_right(nil);
+            nil->set_parent(nil);
+            nil->set_black();
+        }
+        ~red_black_tree(void) { destroy_node(nil); }
+
+        static void left_rotation(NODE *node)
+        {
+            const NODE *pivot = node->get_right();
+            const NODE *parent = node->get_parent();
+
+            /* Swap the position of `node' node and `pivot' node */
+            {
+                node->set_right(pivot->get_left()); /* Change `node.right' to point `pivot.left' */
+                pivot->set_left(node); /* Change `pivot.left' to point `node' */
+                node->set_parent(pivot); /* Change `node.parent' to point `pivot' */
+            }
+
+            /* Change the child of `parent' node */
+            if(parent->get_left() == node)
+                parent->set_left(pivot);
+            else
+                parent->set_right(pivot);
+        }
+
+        static void right_rotation(const NODE *node)
+        {
+            const NODE *pivot = node->get_left();
+            const NODE *parent = node->get_parent();
+
+            /* Swap the position of `node' node and `pivot' node */
+            {
+                node->set_left(pivot->get_right()); /* Change `node.left' to point `pivot.right' */
+                pivot->set_right(node); /* Change `pivot.right' to point `node' */
+                node->set_parent(pivot); /* Change `node.parent' to point `pivot' */
+            }
+
+            /* Change the child of `parent' node */
+            if(parent->get_left() == node)
+                parent->set_left(pivot);
+            else
+                parent->set_right(pivot);
+        }
+
+        static NODE *get_grandparent(const NODE *node)
+        {
+            if(node->get_parent() == nil) return nil;
+            return node->get_parent()->get_parent();
+        }
+
+        static bool is_uncle_red(const NODE *node)
+        {
+            NODE *grandparent = get_grandparent(node);
+
+            if(grandparent->get_left() != node->get_parent())
+                return grandparent->get_left()->is_red();
+            else
+                return grandparent->get_right()->is_red();
+        }
+
+        static void set_uncle_black(const NODE *node)
+        {
+            NODE *grandparent = get_grandparent(node);
+
+            if(grandparent->get_left() != node->get_parent())
+                return grandparent->get_left()->set_black();
+            else
+                return grandparent->get_right()->set_black();
+        }
+
+        static NODE *get_sibling(const NODE *node)
+        {
+            if(node->get_parent()->get_left() == node)
+                return node->get_parent()->get_right();
+            else
+                return node->get_parent()->get_left();
+        }
+
+        void insert_rb(NODE *node)
+        {
+            insert(node); /* Call the BST insertion */
+
+            /* Set the NIL node */
+            if(node->get_left() == NULL) node->set_left(nil);
+            if(node->get_right() == NULL) node->set_right(nil);
+            if(node->get_parent() == NULL) node->set_parent(nil);
+
+            while(node->is_red())
+            {
+                if(node->get_parent()->is_red()) 
+                {
+                    if(is_uncle_red(node))
+                    {
+                        node->get_parent()->set_black();
+                        set_uncle_black(node);
+                        get_grandparent(node)->set_red();
+
+                        node = get_grandparent(node);
+                    }
+                    else
+                    {
+                        if(get_grandparent(node)->get_left() == node->get_parent(node))
+                        {
+                            if(node->get_parent()->get_right() == node)
+                                left_rotation(node->get_parent());
+
+                            right_rotation(node->get_parent());
+                            node->get_right()->set_red();
+                        }
+                        else
+                        {
+                            if(node->get_parent()->get_left() == node)
+                                right_rotation(node->get_parent());
+
+                            left_rotation(node->get_parent());
+                            node->get_left()->set_red();
+                        }
+
+                        node->set_black();
+                        break;
+                    }
+                }
+                else
+                    break;
+            }
+
+            if(root->is_red()) root->set_black();
+        }
+
+        void delete(NODE *node)
+        {
+            if(node == NULL) return; /* Not exists */
+
+            /* Section of BST deletion */
+            {
+            }
+        }
+
+    public:
+        void insert(const DATA &key) { insert_rb(create_node(key)); }
+
     };
 }
