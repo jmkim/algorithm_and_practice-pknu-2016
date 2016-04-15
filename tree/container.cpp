8c5de39798110e6502081a1e976cc9e43febfb38
Jongmin Kim <kdzlvaids@gmail.com>
2016-04-15 13:24:23 +0900

    Container(Red-black tree): Add a deletion method

--- tree/container.cpp
+++ tree/container.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <functional>
 #include <queue>
 
@@ -6,11 +7,12 @@ namespace container
     template<typename DATA> class element
     {
     public:
-        virtual void set(const DATA &key) = 0;
-        virtual DATA get(void) = 0;
+        virtual int set() = 0;
+        virtual DATA get() = 0;
+        virtual bool compare(element *second) = 0; /* Return true if `me' is less than `second' */
     };
 
-    template<class ELEMENT, typename DATA> class node
+    template<class ELEMENT> class node
     {
     protected:
         ELEMENT *me;
@@ -18,81 +20,55 @@ namespace container
         node *right;
 
     public:
-        node(const DATA &key)
-        {
-            me = new ELEMENT(key);
-            left = NULL;
-            right = NULL;
-        }
-        ~node(void) { delete me; }
+        node(void) : me(NULL), left(NULL), right(NULL) {}
+        node(const ELEMENT *element) : me(element), left(NULL), right(NULL) {}
 
-        void set(const DATA &key) { me->set(key); }
+        void set(const ELEMENT *element) { me = element; }
         void set_left(const node *left) { this->left = left; }
         void set_right(const node *right) { this->right = right; }
 
-        DATA get(void) const { return me->get() }
+        ELEMENT *get(void) const { return me; }
         node *get_left(void) const { return left; }
         node *get_right(void) const { return right; }
-    }
+    };
 
-    template<class ELEMENT, typename DATA> class node_p : public node
+    template<class ELEMENT> class node_p : public node<ELEMENT>
     {
     protected:
         node_p *parent;
 
     public:
-        node_p(const DATA &key)
-        {
-            me = new ELEMENT(key);
-            left = NULL;
-            right = NULL;
-            parent = NULL;
-        }
-        node_p(const DATA &key, const node_p *parent)
-        {
-            me = new ELEMENT(key);
-            left = NULL;
-            right = NULL;
-            this->parent = parent;
-        }
-        ~node_p(void) { delete me; }
+        node_p(void) : node<ELEMENT>::node(), parent(NULL) {}
+        node_p(const ELEMENT *element) : node<ELEMENT>::node(element), parent(NULL) {}
+        node_p(const ELEMENT *element, const node_p *parent) : node<ELEMENT>::node(element), parent(parent) {}
 
-        void set_parent(const node_p *parent) { this->right = right; }
+        void set_parent(const node_p *parent) { this->parent = parent; }
         node_p *get_parent(void) const { return parent; }
     };
 
-    template<class ELEMENT, typename DATA> class node_rb : public node_p
+    template<class ELEMENT> class node_rb : public node_p<ELEMENT>
     {
     protected:
-        bool is_color_black;
+        bool color; /* true: red, false: black */
 
     public:
-        node_p(const DATA &key) : is_color_black(false)
-        {
-            me = new ELEMENT(key);
-            left = NULL;
-            right = NULL;
-            parent = NULL;
-        }
-        node_p(const DATA &key, const node_p *parent) : is_color_black(false)
-        {
-            me = new ELEMENT(key);
-            left = NULL;
-            right = NULL;
-            this->parent = parent;
-        }
-        ~node_p(void) { delete me; }
+        node_rb(void) : node_p<ELEMENT>::node_p(), color(true) {}
+        node_rb(const ELEMENT *element) : node_p<ELEMENT>::node_p(element), color(true) {}
+        node_rb(const ELEMENT *element, const node_rb *parent) : node_p<ELEMENT>::node_p(element, parent), color(true) {}
 
-        void set_black(void) { is_color_black = true; }
-        void set_red(void) { is_color_black = false; }
-        bool is_black(void) const { return is_color_black; }
-        bool is_red(void) const { return ! is_color_black; }
+        void set_red(void) { color = true; }
+        void set_black(void) { color = false; }
+        bool is_red(void) const { return color == true; }
+        bool is_black(void) const { return color == false; }
     };
 
-    template<class NODE, typename DATA> class binary_tree
+    template<class NODE, class ELEMENT> class binary_tree
     {
+    public:
+        virtual bool is_empty(void) const = 0;
+
     protected:
-        static NODE *create_node(const DATA &key) { return new NODE(key); }
+        static NODE *create_node(const ELEMENT *element) { return new NODE(element); }
         static void destroy_node(const NODE *node) { delete node; }
 
         static void traverse_inorder(const NODE *const node, const std::function<void(const NODE *const)> callback)
@@ -138,20 +114,32 @@ namespace container
                 if(n->get_right() != NULL) queue.push(n->get_right());
             }
         }
-
-    public:
-        virtual bool is_empty(void) const = 0;
-        virtual int get_size(void) const = 0;
     };
 
-    template<class NODE, typename DATA> class binary_search_tree : public binary_tree<NODE, DATA>
+    template<class NODE, class ELEMENT> class binary_search_tree : public binary_tree<NODE, ELEMENT>
     {
+    public:
+        bool is_empty(void) const { return root == NULL; }
+
+        NODE *search(const ELEMENT *element)
+        {
+            NODE *n = NULL;
+            traverse_inorder(root, [&] (NODE *node) { if(element == node->get()) n = node; return; });
+
+            return n;
+        }
+
+        void insert(const ELEMENT *element) { insert(create_node(element)); }
+        void delete_n(const ELEMENT *element) { NODE *node = search(element); delete_n(node); destroy_node(node); }
+
     protected:
         NODE *root;
-        int size;
 
-        binary_search_tree(void) : size(0) { root = NULL; }
-        ~binary_search_tree(void) { while(! is_empty()) delete(root); }
+        binary_search_tree(void) : root(NULL) {}
+        ~binary_search_tree(void) { while(! is_empty()) delete_n(root); }
+
+        static NODE *create_node(const ELEMENT *element) { return new NODE(element); }
+        static void destroy_node(const NODE *node) { delete node; }
 
         NODE *get_minimum(const NODE *node)
         {
@@ -192,13 +180,13 @@ namespace container
             {
                 p = n;
 
-                if(node->get() < n->get())
+                if(node->get()->compare(n->get()))
                     n = n->get_left();
                 else
                     n = n->get_right();
             }
 
-            if(node->get() < p->get())
+            if(node->get()->compare(p->get()))
                 p->set_left(node);
             else
                 p->set_right(node);
@@ -206,7 +194,7 @@ namespace container
             node->set_parent(p);
         }
 
-        void delete(const NODE *node)
+        void delete_n(const NODE *node)
         {
             if(node == NULL) return; /* Not exists */
 
@@ -242,28 +230,16 @@ namespace container
                 }
             }
         }
-
-    public:
-        bool is_empty(void) const { return root == NULL; }
-        int get_size(void) const { return size; }
-
-        NODE *search(const DATA &key)
-        {
-            NODE *n = NULL;
-            traverse_inorder(root,
-                [&] (NODE *node) => { if(key == node->get()->get()) n = node; return; }
-            );
-
-            return n;
-        }
-
-        void insert(const DATA &key) { insert(create_node(key)); }
-        void delete(const DATA &key) { NODE *node = search(key); delete(node); destroy_node(node); }
     };
 
-    template<class NODE, typename DATA> class red_black_tree : public binary_search_tree<NODE, DATA>
+    template<class NODE, class ELEMENT> class red_black_tree : public binary_search_tree<NODE, ELEMENT>
     {
-    private:
+    public:
+        bool is_empty(void) const { return binary_search_tree<NODE, ELEMENT>::root == nil; }
+        void insert(const ELEMENT *element) { insert_rb(create_node(element)); }
+        void delete_n(const ELEMENT *element) { NODE *node = search(element); delete_rb(node); destroy_node(node); }
+
+    protected:
         NODE *nil;
 
         red_black_tree(void)
@@ -275,6 +251,9 @@ namespace container
             nil->set_black();
         }
         ~red_black_tree(void) { destroy_node(nil); }
+
+        static NODE *create_node(const ELEMENT *element) { return new NODE(element); }
+        static void destroy_node(const NODE *node) { delete node; }
 
         static void left_rotation(NODE *node)
         {
@@ -316,7 +295,6 @@ namespace container
 
         static NODE *get_grandparent(const NODE *node)
         {
-            if(node->get_parent() == nil) return nil;
             return node->get_parent()->get_parent();
         }
 
@@ -396,20 +374,135 @@ namespace container
                     break;
             }
 
-            if(root->is_red()) root->set_black();
+            if(binary_search_tree<ELEMENT, NODE>::root->is_red()) binary_search_tree<ELEMENT, NODE>::root->set_black();
         }
 
-        void delete(NODE *node)
+        void delete_rb(NODE *node)
         {
-            if(node == NULL) return; /* Not exists */
+            if(node == nil) return; /* Not exists */
 
-            /* Section of BST deletion */
+            NODE *del;
+            if(node->get_left() != nil && node->get_right() != nil)
             {
+                del = get_successor(node);
+                node->set(del->get());
             }
+            else
+                del = node;
+
+            NODE *temp;
+            if(node->get_parent()->get_left() == node)
+                temp = del->get_left();
+            else
+                temp = del->get_right();
+
+            NODE *py = del->get_parent();
+
+            delete_n(del); /* Call the BST deletion*/
+
+            if(del->is_red()) return;
+
+            while(temp != binary_search_tree<NODE, ELEMENT>::root && temp->is_black())
+            {
+                NODE *parent;
+                if(temp == nil)
+                    parent = py;
+                else
+                    parent = temp->get_parent();
+
+                NODE *sibling;
+                if(temp == parent->get_left())
+                {
+                    sibling = parent->get_right();
+
+                    if(sibling->is_red())
+                    {
+                        /* CASE ONE */
+                        sibling->set_black();
+                        parent->set_red();
+                        left_rotation(parent);
+                        sibling = parent->get_right();
+                    }
+
+                    if(sibling->get_left()->is_black() && sibling->get_right()->is_black())
+                    {
+                        /* CASE TWO */
+                        sibling->set_red();
+                        temp = parent;
+                    }
+                    else
+                    {
+                        if(sibling->get_right()->is_black())
+                        {
+                            /* CASE THREE */
+                            sibling->set_black();
+                            sibling->set_red();
+                            right_rotation(sibling);
+                            sibling = parent->right();
+                        }
+
+                        /* CASE FOUR */
+                        if(parent->is_black())
+                            sibling->set_black();
+                        else
+                            sibling->set_red();
+
+                        parent->set_black();
+                        sibling->get_right()->set_black();
+
+                        left_rotation(parent);
+
+                        temp = binary_search_tree<NODE, ELEMENT>::root;
+                    }
+                }
+                else
+                {
+                    /* DIRECTION CONVERSION */
+
+                    sibling = parent->get_left();
+                    if(sibling->is_red())
+                    {
+                        sibling->set_black();
+                        parent->set_red();
+
+                        right_rotation(parent);
+
+                        sibling = parent->get_left();
+                    }
+
+                    if(sibling->get_left()->is_black() && sibling->get_right()->is_black())
+                    {
+                        sibling->set_red();
+                        temp = parent;
+                    }
+                    else
+                    {
+                        if(sibling->get_left()->is_black())
+                        {
+                            sibling->get_right->set_black();
+                            sibling->set_red();
+
+                            left_rotation(sibling);
+
+                            sibling = parent->get_left();
+                        }
+
+                        if(parent->is_black())
+                            sibling->set_black();
+                        else
+                            sibling->set_red();
+
+                        parent->set_black();
+                        sibling->get_left()->set_black();
+
+                        right_rotation(parent);
+
+                        temp = binary_search_tree<NODE, ELEMENT>::root;
+                    }
+                }
+            }
+
+            temp->set_black();
         }
-
-    public:
-        void insert(const DATA &key) { insert_rb(create_node(key)); }
-
     };
 }
