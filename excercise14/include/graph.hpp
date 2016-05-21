ba2cf568381587021f32a0ea80197743bae8f930
Jongmin Kim <kdzlvaids@gmail.com>
2016-05-22 01:02:26 +0900

    Add operation functions for graph
    Rename some functions:
     * Adjacent             -> IsEdgeExist
     * GetVertexCount       -> CountOfVertices
     * GetVertexEdgeCount   -> CountOfEdgeOfVertices

--- excercise14/include/graph.hpp
+++ excercise14/include/graph.hpp
@@ -25,9 +25,11 @@ namespace algorithm
     \note   Cannot use double as WeightType
 */
 template<
-    class       ValueType,                            /**< Vertex value type; operator== should be defined */
-    class       WeightType            = unsigned int, /**< Weight type */
-    WeightType  WeightDefaultValue    = 0             /**< Default value of weight */
+    class       ValueType,                                  /**< Vertex value type; operator== should be defined */
+    class       WeightType          = unsigned int,         /**< Weight type */
+    WeightType  WeightDefaultValue  = 0,                    /**< Default value of weight */
+    class       KeyType             = unsigned int,         /**< Key type, used to access an array */
+    class       KeyArrayType        = std::vector<KeyType>  /**< Array of Key type */
 >
 class Graph
 {
@@ -81,25 +83,7 @@ protected:
     std::vector<VertexNode> graph_; /**< Graph */
 
 public:
-    /** Test whether there is an edge from the vertices src to dest
-
-        \return     Return true if the edge exists; otherwise return false
-        \param[in]  key_src         Key of source (src)
-        \param[in]  key_dest        Key of destination (dest)
-    */
-    bool
-    Adjacent(const KeyType & key_src, const KeyType & key_dest)
-    {
-        for(auto & edge : graph_.at(key_src)->edges)
-        {
-            if(IsKeyEqual(edge.first, key_dest) == true) /** Found */
-                return true;
-        }
-
-        return false; /** Not found */
-    }
-
-    /** Add a vertex, if a graph not have the vertex with specified value already
+    /** Add a vertex, if a graph not have the vertex with specific value already
 
         \return     Return the key of vertex if added successfully; otherwise return -1
         \param[in]  value_of_vertex     Value of vertex
@@ -109,7 +93,7 @@ public:
     {
         KeyType key_of_vertex = GetVertexKey(value_of_vertex);
 
-        if(key_of_vertex == GetVertexCount()) /** Not found */
+        if(key_of_vertex == CountOfVertices()) /** Not found */
             graph_.push_back(VertexNode(key_of_vertex, value_of_vertex));
 
         return key_of_vertex;
@@ -129,7 +113,37 @@ public:
         );
     }
 
-    /** Get a key of the vertex with specified value from a graph
+    /** Test whether the key of vertex less than its maximum bound
+
+        \return     Return true if the vertex exists; otherwise return false
+        \param[in]  key_of_vertex   Key of vertex
+    */
+    inline
+    bool
+    IsVertexExist(const KeyType & key_of_vertex)
+    const
+    {
+        try
+        { graph_.at(key_of_vertex); } /** .at() throws exception */
+        catch(std::out_of_range & e)
+        { return false; /** key_of_vertex is over the maximum bound */ }
+
+        return true;
+    }
+
+    /** Test whether there is an edge from the vertices src to dest
+
+        \return     Return true if the edge exists; otherwise return false
+        \param[in]  key_src         Key of source (src)
+        \param[in]  key_dest        Key of destination (dest)
+    */
+    inline
+    bool
+    IsEdgeExist(const KeyType & key_src, const KeyType & key_dest)
+    const
+    { return graph_.at(key_src).edges.find(key_dest) != graph_.at(key_src).edges.end(); }
+
+    /** Get a key of the vertex with specific value
 
         If failed to add, return the size of graph which is an invalid key (maximum key + 1).
 
@@ -145,12 +159,12 @@ public:
                 return vertex.key;
         }
 
-        return GetVertexCount();
+        return CountOfVertices();
     }
 
-    /** Get a value of the vertex with specified key from a graph
+    /** Get a value of the vertex with specific key
 
-        \return     Return the value
+        \return     Value of vertex
         \param[in]  key_of_vertex       Key of vertex
     */
     inline
@@ -159,7 +173,7 @@ public:
     const
     { return graph_.at(key_of_vertex).value; }
 
-    /** Set a value of the vertex with specified key from a graph
+    /** Set a value of the vertex with specific key
 
         \param[in]  key_of_vertex       Key of vertex
         \param[in]  value_of_vertex     Value of vertex
@@ -169,13 +183,36 @@ public:
     SetVertexValue(const KeyType & key_of_vertex, const ValueType & value_of_vertex)
     { graph_.at(key_of_vertex).value = value_of_vertex; }
 
+    /** Get a weight of the edge
+
+        \return     Weight of edge
+        \param[in]  key_src         Key of source (src)
+        \param[in]  key_dest        Key of destination (dest)
+    */
+    inline
+    WeightType
+    GetEdgeWeight(const KeyType & key_src, const KeyType & key_dest)
+    const
+    { return graph_.at(key_src).edges.at(key_dest); }
+
+    /** Set a weight of the edge
+
+        \param[in]  key_src         Key of source (src)
+        \param[in]  key_dest        Key of destination (dest)
+        \param[in]  weight          Weight of edge
+    */
+    inline
+    void
+    SetEdgeWeight(const KeyType & key_src, const KeyType & key_dest, const WeightType & weight)
+    { graph_.at(key_src).edges.at(key_dest) = weight; }
+
     /** Get a count of vertices
 
         \return     Count of vertices
     */
     inline
     SizeType
-    GetVertexCount(void)
+    CountOfVertices(void)
     const
     { return graph_.size(); }
 
@@ -186,9 +223,26 @@ public:
     */
     inline
     SizeType
-    GetVertexEdgeCount(const KeyType & key_of_vertex)
+    CountOfEdgeOfVertices(const KeyType & key_of_vertex)
     const
     { return graph_.at(key_of_vertex).edges.size(); }
+
+    /** Get the list of edges
+
+        \return     Size of list
+        \param[out] out_edges       List of edges
+        \param[in]  key_of_vertex   Key of vertex
+    */
+    SizeType
+    ListOfEdges(KeyArrayType & out_edges, const KeyType & key_of_vertex)
+    {
+        out_edges.clear();
+
+        for(auto & edge : graph_.at(key_of_vertex).edges)
+            out_edges.push_back(edge);
+
+        return out_edges.size();
+    }
 };
 
 } /** ns: algorithm */
